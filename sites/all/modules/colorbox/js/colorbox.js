2924689b286ce9ef7f60de378733aa4ba516f92b
xamanu <drupal@delattre.de>
2013-12-01 10:52:07 -0600

    Adding colorbox module

---
+++ sites/all/modules/colorbox/js/colorbox.js
@@ -0,0 +1,29 @@
+(function ($) {
+
+Drupal.behaviors.initColorbox = {
+  attach: function (context, settings) {
+    if (!$.isFunction($.colorbox)) {
+      return;
+    }
+
+    if (settings.colorbox.mobiledetect && window.matchMedia) {
+      // Disable Colorbox for small screens.
+      mq = window.matchMedia("(max-device-width: " + settings.colorbox.mobiledevicewidth + ")");
+      if (mq.matches) {
+        return;
+      }
+    }
+
+    $('.colorbox', context)
+      .once('init-colorbox')
+      .colorbox(settings.colorbox);
+  }
+};
+
+{
+  $(document).bind('cbox_complete', function () {
+    Drupal.attachBehaviors('#cboxLoadedContent');
+  });
+}
+
+})(jQuery);
