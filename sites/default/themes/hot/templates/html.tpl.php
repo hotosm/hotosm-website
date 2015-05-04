01bcab90be2628031ff319470240af6602915b01
xamanu <drupal@delattre.de>
2015-05-04 07:16:49 -0600

    37: Added Piwik statistics

--- sites/default/themes/hot/templates/html.tpl.php
+++ sites/default/themes/hot/templates/html.tpl.php
@@ -60,6 +60,21 @@
   <?php print $scripts; ?>
 </head>
 <body<?php print $attributes;?>>
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//piwik.hotosm.org/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 5]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//piwik.hotosm.org/piwik.php?idsite=5" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
