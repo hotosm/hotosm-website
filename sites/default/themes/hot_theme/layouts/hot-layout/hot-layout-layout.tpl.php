021b58dd3db46cd45fe09a94d1b9b398a28d0705
xamanu <drupal@delattre.de>
2016-04-30 19:19:51 +0200

    Including Piwik tracking

--- sites/default/themes/hot_theme/layouts/hot-layout/hot-layout-layout.tpl.php
+++ sites/default/themes/hot_theme/layouts/hot-layout/hot-layout-layout.tpl.php
@@ -60,4 +60,21 @@
       <?php print render($page['bottom_footer']); ?>
     </div>
   </footer>
+
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//piwik.hotosm.org/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', 5]);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <noscript><p><img src="//piwik.hotosm.org/piwik.php?idsite=5" style="border:0;" alt="" /></p></noscript>
+  <!-- End Piwik Code -->
+  
 </div>
