833d59bcdb72c25ebfad18ea9e1096a3683e0d5e
Brendan Gatens <howdy@brendangatens.com>
2018-03-05 10:21:23 +0000

    Add sticky nav

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ navAnchor.addEventListener('click', function(e) {
     nav.className = nav.className? '' : 'show-nav';
     e.preventDefault();
 });
+
+var fixmeTop = $('.nav-main').offset().top;
+$(window).scroll(function() {
+  var currentScroll = $(window).scrollTop();
+  if (currentScroll >= fixmeTop) {
+    $('#nav-container').addClass( 'fixed-nav' );
+    $('.nav-logo').addClass( 'fixed-nav' );
+    $('.nav-logo-text').addClass( 'fixed-nav' );
+    $('.nav-main').addClass( 'fixed-nav' );
+  } else {
+    $('#nav-container').removeClass( 'fixed-nav' );
+    $('.nav-logo').removeClass( 'fixed-nav' );
+    $('.nav-logo-text').removeClass( 'fixed-nav' );
+    $('.nav-main').removeClass( 'fixed-nav' );
+  }
+});
