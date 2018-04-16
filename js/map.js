50d166c31d244e709a3b7fd960a86995683b4ba8
Wille Marcel <wille@wille.blog.br>
2018-04-16 18:51:37 -0300

    add map to 'Our Work' page

---
+++ js/map.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaG90IiwiYSI6IlBtUmNiR1kifQ.dCS1Eu9DIRNZGktc24IwtA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    logoPosition: 'bottom-right',
+    scrollZoom: false,
+    zoom: 1.25,
+    center: [0, 25],
+    style: 'mapbox://styles/hot/cjepk5hhz5o9w2rozqj353ut4'
+});
