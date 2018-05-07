ad28c92647992f6f9bd1e898a8ac8fa351e59a72
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-05-07 23:12:08 +0530

    buildings snafu

--- js/countries-stats.js
+++ js/countries-stats.js
@@ -1,10 +1,8 @@
 // country level stats from Missing Maps
-console.log(missingMapsCode);
 $(document).ready(function(){
   $.get("https://osm-stats-production-api.azurewebsites.net/countries/" + missingMapsCode, function(data){
-    console.log(data)
     $('#Total-Map-Edits').text(formatedData(data['all_edits']));
-    $('#Buildings-Mapped').text(formatedData(data['building_count_add']) + formatedData(data['building_count_mod']));
+    $('#Buildings-Mapped').text(formatedData(data['building_count_add'] + data['building_count_mod']));
     $('#Roads-Mapped').text(formatedData(Math.round(parseInt(data['road_km_add']) + parseInt(data['road_km_mod']))));
     $('#Community-Mappers').text(formatedData(data['contributors']));
   });
