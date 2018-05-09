dcdd4de717bc12ee2ec3cbdc0769873376dcc458
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-05-09 21:31:05 +0530

    center loading + home page loading integration

--- js/countries-stats.js
+++ js/countries-stats.js
@@ -1,11 +1,11 @@
 // country level stats from Missing Maps
 $(document).ready(function () {
-  $('.loader').css('display', 'block');
+  $('.loader').show();
   $.get('https://osm-stats-production-api.azurewebsites.net/countries/' + missingMapsCode, function (data) {
     $('#Total-Map-Edits').text(formatedData(data['all_edits']));
     $('#Buildings-Mapped').text(formatedData(data['building_count_add'] + data['building_count_mod']));
     $('#Roads-Mapped').text(formatedData(Math.round(parseInt(data['road_km_add']) + parseInt(data['road_km_mod']))));
     $('#Community-Mappers').text(formatedData(data['contributors']));
-    $('.loader').css('display', 'none');
+    $('.loader').hide();
   });
 });
--- js/stats.js
+++ js/stats.js
@@ -1,12 +1,20 @@
 // add stats to the home page
 $(document).ready(function(){
+  $('#mappers').show();
+  $('#edits').show();
+  $('#buildings').show();
+  $('#roads').show();
   $.get("https://tasks.hotosm.org/api/v1/stats/home-page", function(data){
     $('#Community-Mappers').text(formatedData(data['totalMappers']));
+    $('#mappers').hide();
   });
 
   $.get("https://osm-stats-production-api.azurewebsites.net/stats/missingmaps", function(data){
     $('#Total-Map-Edits').text(formatedData(data['edits']));
     $('#Buildings-Mapped').text(formatedData(data['buildings']));
     $('#Roads-Mapped').text(formatedData(Math.round(parseInt(data['roads']))));
+    $('#edits').hide();
+    $('#buildings').hide();
+    $('#roads').hide();
   });
 });
