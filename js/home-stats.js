a5d8b1f5dffad7609c8ab7a852511e1622c9d36f
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-05-28 16:33:10 +0530

    remove console log

--- js/home-stats.js
+++ js/home-stats.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     $('.loader').show();
     $.get('https://tasks.hotosm.org/api/v1/stats/home-page', function (data) {
-      console.log(data)
       $('#Community-Mappers').text(formatedData(data['totalMappers']));
       $('#Online-Mappers').text(formatedData(data['mappersOnline']));
       $('#Tasks-Mapped').text(formatedData(data['tasksMapped']));
