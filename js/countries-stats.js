1c44f95315bf48605b7bc3a5ae244c6e72d4b61b
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-06-18 12:11:29 +0530

    handle non existent countries

--- js/countries-stats.js
+++ js/countries-stats.js
@@ -1,9 +1,7 @@
 // country level stats from stats-collector
-var edits = 0; 
-var mappers = 0; 
-var buildings = 0;
-var roads = 0;
+var stats = {'edits': 0, 'mappers': 0, 'buildings': 0, 'roads': 0};
 var country = $(document).find("title").text().split('|')[1].trim();   
+if (country.startsWith('United')) country = 'United States of America'
 
 $(document).ready(function () {
   $('.loader').show()
@@ -13,21 +11,31 @@ $(document).ready(function () {
   })
   .then(function (jsonData) {
     console.log(jsonData)
-    jsonData[country].forEach(project => {
-      edits += project.properties['edits']
-      mappers += project.properties['mappers']
-      buildings += project.properties['buildings']
-      roads += project.properties['roads']
-    })
-    console.log('edits: ', edits)
-    console.log('mappers: ', mappers)
-    console.log('buildings: ', buildings)
-    console.log('roads: ', roads)
-    $('#Total-Map-Edits').text(formatedData(edits))
-    $('#Buildings-Mapped').text(formatedData(buildings))
-    $('#Roads-Mapped').text(formatedData(Math.round(parseInt(roads))))
-    $('#Community-Mappers').text(formatedData(mappers))
-    $('.loader').hide()
+    if (jsonData[country]) {
+      jsonData[country].forEach(project => {
+        stats.edits += project.properties['edits']
+        stats.mappers += project.properties['mappers']
+        stats.buildings += project.properties['buildings']
+        stats.roads += Math.round(project.properties['roads'])
+      })
+      updateStats(stats)
+    } else {
+      $.get('https://osm-stats-production-api.azurewebsites.net/countries/' + missingMapsCode, function (data) {
+        stats.edits = formatedData(data['all_edits']);
+        stats.buildings = formatedData(data['building_count_add'] + data['building_count_mod']);
+        stats.roads = formatedData(Math.round(parseInt(data['road_km_add']) + parseInt(data['road_km_mod'])));
+        stats.mappers = formatedData(data['contributors']);
+        updateStats(stats)
+      });
+    }
   }
   )
 })
+
+function updateStats (stats) {
+  $('#Total-Map-Edits').text(stats.edits);
+  $('#Buildings-Mapped').text(stats.buildings);
+  $('#Roads-Mapped').text(stats.roads);
+  $('#Community-Mappers').text(stats.mappers);
+  $('.loader').hide();
+}
