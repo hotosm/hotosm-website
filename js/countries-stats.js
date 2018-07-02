6e8c9288151f041a717b683161e0d0f4650f0563
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-07-02 10:36:47 +0530

    #327 re-organise code

--- js/countries-stats.js
+++ js/countries-stats.js
@@ -1,6 +1,6 @@
 // country level stats from stats-collector
 var stats = {'edits': 0, 'mappers': 0, 'buildings': 0, 'roads': 0};
-var country = $(document).find("title").text().split('|')[1].trim();   
+var country = $(document).find('title').text().split('|')[1].trim();   
 if (country.startsWith('United')) country = 'United States of America'
 
 $(document).ready(function () {
@@ -31,33 +31,26 @@ $(document).ready(function () {
   )
 })
 
-function switchTab(evt, cityName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-      console.log(tabcontent[i])
-      tabcontent[i].style.display = "none";
-  }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-      console.log(tablinks[i])
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "flex";
-  evt.currentTarget.className += " active";
-}
-
 function updateStats (stats) {
   $('#Total-Map-Edits').text(formatedData(stats.edits));
   $('#Buildings-Mapped').text(formatedData(stats.buildings));
   $('#Roads-Mapped').text(formatedData(stats.roads));
   $('#Community-Mappers').text(formatedData(stats.mappers));
   $('.loader').hide();
+}
+
+function switchTab(evt, tabName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName('tabcontent');
+  for (i = 0; i < tabcontent.length; i++) {
+      console.log(tabcontent[i])
+      tabcontent[i].style.display = 'none';
+  }
+  tablinks = document.getElementsByClassName('tablinks');
+  for (i = 0; i < tablinks.length; i++) {
+      console.log(tablinks[i])
+      tablinks[i].className = tablinks[i].className.replace(' active', '');
+  }
+  document.getElementById(tabName).style.display = 'flex';
+  evt.currentTarget.className += ' active';
 }
