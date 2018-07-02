54c1f11d736576443c752a7d003cbc3f88255013
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-07-02 10:34:16 +0530

    #327 create tabs for stats

--- js/countries-stats.js
+++ js/countries-stats.js
@@ -31,6 +31,29 @@ $(document).ready(function () {
   )
 })
 
+function switchTab(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      console.log(tabcontent[i])
+      tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      console.log(tablinks[i])
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
 function updateStats (stats) {
   $('#Total-Map-Edits').text(formatedData(stats.edits));
   $('#Buildings-Mapped').text(formatedData(stats.buildings));
