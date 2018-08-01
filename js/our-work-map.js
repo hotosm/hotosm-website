d0dd195179b30412331d4ef84b7ca63b29de08b0
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-08-01 17:51:43 +0530

    #292 project link in hover + adjust hover behaviour

--- js/our-work-map.js
+++ js/our-work-map.js
@@ -392,9 +392,15 @@ var popup = new mapboxgl.Popup({
         $("#hover-details").empty();
         $("#hover-details").removeClass('hide');
         $("#hover-details").append(
-          '<p class="hover-name">' + projectHover[0].properties.title + '</p>' +
+          '<p class="hover-name">' + 
+          '<a target="_blank" href="https://tasks.hotosm.org/project/' +
+          projectHover[0].properties.id + 
+          '">#' + projectHover[0].properties.id +'</a>' +
+           " - " +
+          projectHover[0].properties.title + '</p>' +
           '<p class= "hover-edits">' + formatedData(projectHover[0].properties.edits) + ' Edits </p>' 
         );
+        console.log()
         var coordinates = projectHover[0].geometry.coordinates.slice();
         var description = "<html><h6><a target='_blank' href='https://tasks.hotosm.org/project/" + projectHover[0].properties.id
                           + "'</a>#" + projectHover[0].properties.id + " - "
@@ -403,15 +409,15 @@ var popup = new mapboxgl.Popup({
             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
 
-        popup.setLngLat(coordinates)
-            .setHTML(description)
-            .addTo(map);
+        // popup.setLngLat(coordinates)
+        //     .setHTML(description)
+        //     .addTo(map);
         
       } else {
         map.getCanvas().style.cursor = '';
-        $("#hover-details").empty();
-        $("#hover-details").addClass('hide');
-        popup.remove();
+        // $("#hover-details").empty();
+        // $("#hover-details").addClass('hide');
+        // popup.remove();
       }
     }
   });
@@ -450,13 +456,15 @@ function countryTabSwitch(evt, tabName) {
   var projectLayers = ['all-projects-symbol', 'all-projects-black-circle', 'all-projects-edits-circle', 'all-projects-clusters']
   var countryLayers = ['project_countries', 'member_countries', 'centroids_project_countries', 'centroids_member_countries', 'active_centroids_project_countries', 'active_centroids_project_countries_pulse']
   var countryLegends = ['prefix-legend', 'projects-legend', 'active-legend', 'member-legend']
-  var projectLegends = ['project-area-legend']
+  var projectLegends = ['cluster-legend', 'tm-legend']
   tablinks = document.getElementsByClassName('tablinks');
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(' active', '');
   }
   evt.currentTarget.className += ' active';
   if (tabName === 'project-map') {
+    $("#hover-details").empty();
+    $("#hover-details").addClass('hide');
     $('#expand-collapse').removeClass('hide');
     for (let i =0; i<projectLegends.length; i++){
       $('#'+projectLegends[i]).removeClass('hide');
@@ -471,6 +479,8 @@ function countryTabSwitch(evt, tabName) {
       map.setLayoutProperty(countryLayers[j], 'visibility', 'none')
     }
   } else if (tabName === 'country-map') {
+    $("#hover-details").empty();
+    $("#hover-details").addClass('hide');
     $('#expand-collapse').addClass('hide');
     for (let i =0; i<countryLegends.length; i++){
       $('#'+countryLegends[i]).removeClass('hide');
@@ -486,39 +496,4 @@ function countryTabSwitch(evt, tabName) {
     }}
 }
 
-function legend(event, legend){
-  if (legend === 'cluster'){
-    if (event === 'expand'){
-      // $('#legend-sep').removeClass('hide')
-      $('#lone-project-legend').addClass('hide')
-      $('#cluster-detailed').removeClass('hide')
-      $('#cluster-collapse').removeClass('hide')
-      $('#cluster-expand').addClass('hide')
-    } else {
-      $('#lone-project-legend').removeClass('hide')
-      $('#cluster-detailed').addClass('hide')
-      $('#cluster-collapse').addClass('hide')
-      $('#cluster-expand').removeClass('hide')
-    }
-    
-  } else {
-    if (event === 'expand'){
-      console.log('Give details')
-      $('#cluster-legend').addClass('hide')
-      $('#collapse').removeClass('hide')
-      $('#expand').addClass('hide')
-      $('#lone-project-detailed').removeClass('hide')
-      // $('#legend-sep').removeClass('hide')
-    } else {
-      console.log('Hide details')
-      $('#cluster-legend').removeClass('hide')
-      $('#expand').removeClass('hide')
-      $('#collapse').addClass('hide')
-      $('#lone-project-detailed').addClass('hide')
-      // $('#legend-sep').addClass('hide')
-    }
-  }
-  
-  
-}
 
