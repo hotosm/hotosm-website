07004c36802aaf6d7e696041a0e6adcf0e419147
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-10-18 20:20:06 +0700

    #187, #128, #335 Add a read more option to the content section

--- js/project-map.js
+++ js/project-map.js
@@ -126,5 +126,16 @@ map.on('load', function () {
     }
   }, 'place-city-sm');
 
- 
 });
+
+function content(action) {
+  if (action === 'more') {
+    document.getElementById("panel").style.height="100%"
+    document.getElementById("less").style.display="block"
+    document.getElementById("more").style.display="none"
+  } else {
+    document.getElementById("panel").style.height="100px"
+    document.getElementById("more").style.display="block"
+    document.getElementById("less").style.display="none"
+  }
+}
