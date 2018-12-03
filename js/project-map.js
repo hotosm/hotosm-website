c5f41c93670a6afc7cb426171219ec5ae79452a5
Ramya Ragupathy <r.ragupathy85@gmail.com>
2018-12-03 11:34:06 +0530

    #187, #128, #335, #287, #340 Improve data sourcing

--- js/project-map.js
+++ js/project-map.js
@@ -1,8 +1,16 @@
 var allProjects = {}
+var projectList = []
+var options = {
+  headers: {
+    'Accept': 'application/json',
+    'Accept-Language': 'en'
+  }
+}
 var tmProjectCentroids = {
   'type': 'FeatureCollection',
   'features': []
 }
+
 var tmProjectPolygons = {
   'type': 'FeatureCollection',
   'features': []
@@ -32,150 +40,63 @@ var map = new mapboxgl.Map({
   style: 'mapbox://styles/hot/cjepk5hhz5o9w2rozqj353ut4'
 })
 
-const loadMapLayers = (boxZoom) => {
-  fetch('/campaigns-boundaries.json')
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (jsonData) {
-    if (campaignTags[0] !== ""){
-    campaignTags.forEach(campaignTag => {
-      if (jsonData[campaignTag]) {
-        jsonData[campaignTag].forEach(campaignProject => {
-          tmProjectPolygons.features.push(campaignProject)
-        })
-      }
-    })
-    console.log('Total Project Boundaries: ', tmProjectPolygons.features.length)
-    var polygonZoom = 3.25
-    if (boxZoom) {
-      polygonZoom = boxZoom + 2
-    }
-    map.addSource('tmProjectPolygons', {
-      'type': 'geojson',
-      'data': tmProjectPolygons
-    })
-  
-    map.addLayer({
-      'id': 'tm-projects-polygons',
-      'type': 'fill',
-      'source': 'tmProjectPolygons',
-      'minzoom': polygonZoom,
-      'maxzoom': 19,
-      'paint': {
-        'fill-opacity': 0.2,
-        'fill-color': '#000000'
-      }
-    }, 'place-city-sm')
-  } else {
-    if (boxZoom){
-      map.setMaxZoom(boxZoom)
-    } else map.setMaxZoom(1.25)
-  }
-  })
 
-fetch('/campaigns-centroids.json')
-  .then(function (response) {
-    return response.json()
+const loadMapLayers = () => {
+  console.log('Load Map Layers')
+  console.log('loadMapLayers() tmProjectPolygons: ', JSON.stringify(tmProjectPolygons))
+  console.log('loadMapLayers() tmProjectCentroids: ', JSON.stringify(tmProjectCentroids))
+  map.addSource('tmProjectPolygons', {
+    'type': 'geojson',
+    'data': tmProjectPolygons
   })
-  .then(function (jsonData) {
-    allProjects = jsonData
-    if (campaignTags[0] !== ""){
-    campaignTags.forEach(campaignTag => {
-      campaignTag = campaignTag.trim()
-      if (allProjects[campaignTag]) {
-        allProjects[campaignTag].forEach(campaignProject => {
-          tmProjectCentroids.features.push(campaignProject)
-          totalArea += campaignProject.properties.area
-          totalEdits += campaignProject.properties.edits
-          totalMappers += campaignProject.properties.mappers
-          totalChangesets += campaignProject.properties.changesets
-          totalRoads += campaignProject.properties.roads
-          totalBuildings += campaignProject.properties.buildings
-        })
-      }
-
-      console.log('Total Project Centroids: ', tmProjectCentroids.features.length)
-      console.log('Total Area: ', totalArea)
-      console.log('Total Buildings: ', totalBuildings)
-      console.log('Total Roads: ', totalRoads)
-      console.log('Total Mappers: ', totalMappers)
-      console.log('Total Edits: ', totalEdits)
-      console.log('Total Changesets: ', totalChangesets)
-      console.log('Total Countries: ', countryList.length)
-      document.getElementById('Project-Area').innerHTML = formatedData(Math.round(totalArea))
-      document.getElementById('Total-Map-Edits').innerHTML = formatedData(Math.round(totalEdits))
-      document.getElementById('Community-Mappers').innerHTML = formatedData(Math.round(totalMappers))
-      document.getElementById('Countries-Covered').innerHTML = countryList.length
-      
-    })
-    map.addSource('tmProjectCentroids', {
-      'type': 'geojson',
-      'data': tmProjectCentroids
-    })
-    
+  map.addSource('tmProjectCentroids', {
+    'type': 'geojson',
+    'data': tmProjectCentroids
+  })
   
-    // map.addLayer({
-    //   'id': 'tm-projects-edits-circle',
-    //   'type': 'circle',
-    //   'source': 'tmProjectCentroids',
-    //   'minzoom': 0,
-    //   'maxzoom': 19,
-    //   'paint': {
-    //     'circle-radius': {
-    //       property: 'edits',
-    //       stops: [
-    //         [0, 10],
-    //         [10000, 15],
-    //         [50000, 20],
-    //         [100000, 25],
-    //         [125000, 30],
-    //         [150000, 35],
-    //         [200000, 40]
-  
-    //       ]
-    //     },
-    //     'circle-opacity': 0.7,
-    //     'circle-color': '#FFC151'
-    //   }
-    // }, 'place-city-sm')
-    var centroidZoom = 1.25
-    if (boxZoom){
-      centroidZoom = boxZoom
+  map.addLayer({
+    'id': 'tm-projects-polygons',
+    'type': 'fill',
+    'source': 'tmProjectPolygons',
+    'minzoom': 0,
+    'maxzoom': 19,
+    'paint': {
+      'fill-opacity': 0.2,
+      'fill-color': '#000000'
     }
-    map.addLayer({
-      'id': 'tm-projects-black-circle',
-      'type': 'circle',
-      'source': 'tmProjectCentroids',
-      'minzoom': centroidZoom,
-      'maxzoom': 19,
-      'paint': {
-        'circle-radius': 5,
-        'circle-opacity': 0.7,
-        'circle-color': '#000000'
-      }
-    }, 'place-city-sm')
+  }, 'place-city-sm')
   
-    map.addLayer({
-      'id': 'tm-projects-symbol',
-      'type': 'symbol',
-      'source': 'tmProjectCentroids',
-      'minzoom': centroidZoom,
-      'maxzoom': 19,
-      'layout': {
-        'text-field': '+',
-        'text-font': ['Open Sans Bold'],
-        'text-offset': [-0.001, -0.03],
-        'text-size':10
-      },
-      'paint': {
-        'text-color': '#FFFFFF'
-      }
-    }, 'place-city-sm')
-  }
-  })
+  map.addLayer({
+    'id': 'tm-projects-black-circle',
+    'type': 'circle',
+    'source': 'tmProjectCentroids',
+    'minzoom': 0,
+    'maxzoom': 19,
+    'paint': {
+      'circle-radius': 5,
+      'circle-opacity': 0.7,
+      'circle-color': '#000000'
+    }
+  }, 'place-city-sm')
+  
+  map.addLayer({
+    'id': 'tm-projects-symbol',
+    'type': 'symbol',
+    'source': 'tmProjectCentroids',
+    'minzoom': 0,
+    'maxzoom': 19,
+    'layout': {
+      'text-field': '+',
+      'text-font': ['Open Sans Bold'],
+      'text-offset': [-0.001, -0.03],
+      'text-size': 10
+    },
+    'paint': {
+      'text-color': '#FFFFFF'
+    }
+  }, 'place-city-sm')
+  
 }
-
 
 map.on('load', function () {
   $('.mapboxgl-ctrl').addClass('hide')
@@ -185,73 +106,101 @@ map.on('load', function () {
     'url': 'mapbox://hot.6w45pyli'
   })
   
-  if (countryList.length < 2) {
-    fetch('/js/bbox.json')
-      .then(function (response) {
-        return response.json()
-    })
-      .then(function (jsonData) {
-        var bboxCountries = jsonData
-        var countryCode
-        for (bbox in bboxCountries) {
-        // console.log(bbox)
-        // console.log(countries.split(','))
-        // console.log(bboxCountries[bbox][0])
-          if (countries.split(',')[0] === bboxCountries[bbox][0]) {
-            countryCode = bbox
-            console.log('countryCode: ', countryCode)
-          }
-        }
-        map.fitBounds(bboxCountries[countryCode][1], {
-        }, setTimeout(() => {
-          var boxZoom = map.getZoom()
-          map.setMinZoom(boxZoom)
-          map.addLayer({
-            "id": "active_countries",
-            "type": "fill",
-            "source": "countriesbetter",
-            "source-layer": "countries-polygon-7jl2br",
-            "minzoom": 0,
-            "maxzoom": 8,
-            "filter": ['any', ['in', 'name_low'].concat(countryList)],
-            "paint": {
-              "fill-pattern": "lines-red-4",
-              "fill-outline-color": "#EFB4B4"
-            }
-          }, 'place-city-sm');
-          loadMapLayers(boxZoom)
-        }, 2000))
-       
-      }
-      )
-  }
-  else {
-    map.addLayer({
-      "id": "active_countries",
-      "type": "fill",
-      "source": "countriesbetter",
-      "source-layer": "countries-polygon-7jl2br",
-      "minzoom": 0,
-      "maxzoom": 8,
-      "filter": ['any', ['in', 'name_low'].concat(countryList)],
-      "paint": {
-        "fill-pattern": "lines-red-4",
-        "fill-outline-color": "#EFB4B4"
-      }
-    }, 'place-city-sm');
-    loadMapLayers()
-  }
+  
 })
 
-function content (action) {
-  if (action === 'more') {
-    document.getElementById('panel').style.height = '100%'
-    document.getElementById('less').style.display = 'block'
-    document.getElementById('more').style.display = 'none'
-  } else {
-    document.getElementById('panel').style.height = '100px'
-    document.getElementById('more').style.display = 'block'
-    document.getElementById('less').style.display = 'none'
-  }
-}
+fetch('/allProjects-minified-v2.json')
+  .then(function (response) {
+    return response.json()
+  })
+  .then(function (jsonData) {
+    allProjects = jsonData
+    fetch('/campaign-match.json')
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (jsonData) {
+        campaignTags.forEach(campaignTag => {
+          if (jsonData[campaignTag]) {
+            projectList = projectList.concat(jsonData[campaignTag])
+          }
+        })
+        var totalProjects = projectList.length
+        projectList.forEach((project, projectCount) => {
+          var feature = {
+            'type': 'Feature',
+            'properties': {},
+            'geometry': {}
+          }
+          feature.properties['id'] = project
+          feature.properties['name'] = allProjects[project][0]
+          feature.properties['status'] = allProjects[project][1]
+          feature.properties['campaignTag'] = allProjects[project][2]
+          feature.properties['created'] = allProjects[project][3]
+          feature.properties['lastUpdated'] = allProjects[project][4]
+          feature.properties['lastActive'] = allProjects[project][6]
+          feature.properties['changesets'] = allProjects[project][7]
+          feature.properties['mappers'] = allProjects[project][8]
+          feature.properties['roads'] = allProjects[project][9]
+          feature.properties['buildings'] = allProjects[project][10]
+          feature.properties['edits'] = allProjects[project][11]
+          
+          totalEdits += allProjects[project][11]
+          totalMappers += allProjects[project][8]
+          totalChangesets += allProjects[project][7]
+          totalRoads += allProjects[project][9]
+          totalBuildings += allProjects[project][10]
+          feature.geometry['type'] = 'Point'
+          feature.geometry['coordinates'] = allProjects[project][5]
+          // console.log('From minified file: ', allProjects[project][5])
+          // console.log('Point geometry: ', feature.geometry['coordinates'])
+          // console.log('Point Feature: ', JSON.stringify(feature))
+          // console.log('before pushing: ',JSON.stringify(tmProjectCentroids))
+          // tmProjectCentroids.features.push(feature)
+          // console.log('After pushing: ', JSON.stringify(tmProjectCentroids))
+          // map.getSource('tmProjectCentroids').setData(tmProjectCentroids)
+          options.url = 'https://s3.amazonaws.com/hotosm-stats-collector/' + project + '-aoi.json'
+          // console.log('Fetching from, ', options.url)
+          $.ajax({
+            url: options.url,
+            type: 'GET',
+            dataType: 'json',
+            success: function (result) {
+              var polygonFeature = {
+                'type': 'Feature',
+                'properties': {},
+                'geometry': {}
+              }
+              var aoi = result
+              // console.log(typeof aoi)
+              aoi = JSON.parse(JSON.stringify(aoi))
+              // console.log(typeof aoi)
+              // console.log(aoi.properties)
+              polygonFeature.properties['area'] = aoi.properties.area
+              totalArea += aoi.properties.area
+              polygonFeature.geometry = aoi.geometry
+              // console.log('Polygon geometry: ',aoi.geometry)
+              tmProjectPolygons.features.push(polygonFeature)
+              // map.getSource('tmProjectPolygons').setData(tmProjectPolygons)
+              
+              // console.log('projectCount: ', projectCount)
+              if (projectCount === (totalProjects - 1)){
+                // console.log('Last project reached')
+                document.getElementById('Project-Area').innerHTML = formatedData(Math.round(totalArea))
+                document.getElementById('Total-Map-Edits').innerHTML = formatedData(Math.round(totalEdits))
+                document.getElementById('Community-Mappers').innerHTML = formatedData(Math.round(totalMappers))
+                document.getElementById('Countries-Covered').innerHTML = countryList.length
+                // console.log('tmProjectPolygons: ', tmProjectPolygons)
+                // console.log('tmProjectCentroids: ', tmProjectCentroids)
+                loadMapLayers()
+              }
+            },
+            error: function (error) {
+              console.log('Error')
+            }
+          })
+        })
+      })
+  })
+
 
