b530381dc6c69032864dbab2ed73ee5b58574ed0
Ekaterina Utochkina <ekaterina.utochkina.2@gmail.com>
2019-08-08 10:44:04 +0300

    Remove redundant code

--- js/countries-map.js
+++ js/countries-map.js
@@ -80,20 +80,6 @@ $('#country-details').height(mapHeight);
 $('#country-filters').height(mapHeight);
 map.resize();
 
-function switchInfo(evt, tabName) {
-  var i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName('detailsTabContent');
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = 'none';
-  }
-  tablinks = document.getElementsByClassName('detailsTabLinks');
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(' active', '');
-  }
-  document.getElementById(tabName).style.display = 'block';
-  evt.currentTarget.className += ' active';
-}
-
 map.on('load', function () {
   map.addSource('countriesbetter', {
     "type": "vector",
